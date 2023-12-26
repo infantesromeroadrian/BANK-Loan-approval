39e95a953cfbf35bd89ab7b9b15ace63838abb2e
adrianinfantes <infantesromeroadrian@gmail.com>
2023-12-26 21:47:26 +0100

    Add py directory

--- py/model_tester.py
+++ py/model_tester.py
@@ -6,6 +6,14 @@ from sklearn.model_selection import GridSearchCV
 import mlflow
 from mlflow.sklearn import log_model
 
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, VotingClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
+from sklearn.model_selection import GridSearchCV
+import mlflow
+from mlflow.sklearn import log_model
+
 class ModelTester:
     def __init__(self, X_train, X_test, y_train, y_test):
         self.X_train = X_train
@@ -17,26 +25,28 @@ class ModelTester:
         with mlflow.start_run():
             model.fit(self.X_train, self.y_train)
             y_pred = model.predict(self.X_test)
-            y_pred_proba = model.predict_proba(self.X_test)[:, 1]  # Obtener probabilidades de la clase positiva
+
+            # Si el modelo tiene un método predict_proba, úsalo para calcular el AUC
+            if hasattr(model, "predict_proba"):
+                y_score = model.predict_proba(self.X_test)[:, 1]  # Asume que la segunda columna es la probabilidad de la clase positiva
+                auc = roc_auc_score(self.y_test, y_score)
+            else:
+                auc = "No disponible"  # O maneja de otra manera si el modelo no soporta predict_proba
 
             accuracy = accuracy_score(self.y_test, y_pred)
-            f1 = f1_score(self.y_test, y_pred, pos_label='Y')
-            auc = roc_auc_score(self.y_test, y_pred_proba)  # Usar probabilidades aquí
-
+            f1 = f1_score(self.y_test, y_pred, pos_label='Y')  # Asegúrate de que 'Y' es tu etiqueta positiva
 
             print(f"Resultados para {model_name}:")
-            print(f"  Precisión: {accuracy:.2f}, F1-Score: {f1:.2f}, AUC: {auc:.2f}")
-
+            print(f"  Precisión: {accuracy:.2f}, F1-Score: {f1:.2f}, AUC: {auc}")
 
             mlflow.log_param("model_name", model_name)
             mlflow.log_metric("accuracy", accuracy)
             mlflow.log_metric("f1_score", f1)
-            mlflow.log_metric("auc", auc)
+            if auc != "No disponible":
+                mlflow.log_metric("auc", auc)
 
             # Ruta para guardar el modelo
             model_path = "model"
-
-            # Guardar el modelo en MLflow
             mlflow.sklearn.log_model(model, artifact_path=model_path, registered_model_name=model_name)
 
 
